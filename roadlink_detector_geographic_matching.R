3a6501db9b17c9f37819bd3633771b7cc52d5331
turanjade <ran.tu@alumni.utoronto.ca>
2025-04-22 21:51:36 -0500

    initial commit

---
+++ roadlink_detector_geographic_matching.R
@@ -0,0 +1,246 @@
+library('arcgisbinding')
+library('sf')
+library('dplyr')
+
+setwd("C:\\Users\\rtu\\OneDrive - The North Central Texas Council of Governments\\Documents\\0_ModelDataDevelopment")
+
+sidefire2025 = st_read('20250410_capacity_recalculation\\RoadNetwork_2026\\ArcGIS\\Sidefire_2025\\sidefire_2025.shp')
+roadlink_2026 = st_read('20250410_capacity_recalculation\\RoadNetwork_2026\\ArcGIS\\RDWY\\road_shp_addedinfo.shp')
+taz_2026 = st_read('20250410_capacity_recalculation\\RoadNetwork_2026\\ArcGIS\\TAZ\\taz_shp.shp')
+
+sidefire_2025 <- sidefire2025 %>%
+  select(ID, LINKNAME, FUNCL, DIR, geometry)
+# colnames(sidefire_2025) = c('ID', 'LINKNAME', 'FUNCL', 'DIR', 'geometry')
+
+st_crs(sidefire_2025) == st_crs(roadlink_2026)  # should be TRUE, check if two layers are in the same coordinate
+st_crs(sidefire_2025) == st_crs(taz_2026) # should be TRUE, check if two layers are in the same coordinate
+
+
+# find the nearest line of each point
+nearest_line_indices <- st_nearest_feature(sidefire_2025, roadlink_2026)
+nearest_lines <- roadlink_2026[nearest_line_indices, ]
+
+sidefire_2025$nearlink_id_26 = nearest_lines$ID
+sidefire_2025$nearlink_name_26 = nearest_lines$STREET
+sidefire_2025$nearlink_funcl_26 = nearest_lines$FUNCL
+
+# calculate the nearest distance
+distances <- st_distance(sidefire_2025, nearest_lines, by_element = TRUE)
+sidefire_2025$distance_meters_26 <- as.numeric(distances)
+
+### check Francisco's method
+# mark distance > 100ft (1)
+sidefire_2025$distance_notmatch_26 = 0
+sidefire_2025$distance_notmatch_26[which(sidefire_2025$distance_meters_26 > 30.48)] = 1
+length(which(sidefire_2025$distance_notmatch_26 == 1))
+
+# mark link FUNCL not match (1)
+sidefire_2025$funcl_notmatch = 0
+for (i in 1:nrow(sidefire_2025)) {
+  if (sidefire_2025$FUNCL[i] != roadlink_2026$FUNCL[which(roadlink_2026$ID == sidefire_2025$nearlink_id_26[i])]) {
+    sidefire_2025$funcl_notmatch[i] = 1
+  }
+}
+length(which(sidefire_2025$funcl_notmatch == 1))
+
+# mark direction not match (1)
+# NB, SB, EB, WB
+NB_match = grepl(" NB", sidefire_2025$LINKNAME) * grepl(" NB", sidefire_2025$near_line_name_26)
+SB_match = grepl(" SB", sidefire_2025$LINKNAME) * grepl(" SB", sidefire_2025$near_line_name_26)
+EB_match = grepl(" EB", sidefire_2025$LINKNAME) * grepl(" EB", sidefire_2025$near_line_name_26)
+WB_match = grepl(" WB", sidefire_2025$LINKNAME) * grepl(" WB", sidefire_2025$near_line_name_26)
+
+# check if NB, SB, EB, WB match
+sidefire_2025$dir_notmatch_26 = 0
+for (i in 1:nrow(sidefire_2025)) {
+  # count name contains NB but road link name does not
+  if (isTRUE(grepl("NB", sidefire_2025$LINKNAME[i]))) {
+    if (isFALSE(grepl('NB', sidefire_2025$nearlink_name_26[i]))) {
+      sidefire_2025$dir_notmatch_26[i] = 1
+    }
+  } 
+  # count name contains SB but road link name does not
+  else if (isTRUE(grepl("SB", sidefire_2025$LINKNAME[i]))) {
+    if (isFALSE(grepl('SB', sidefire_2025$nearlink_name_26[i]))) {
+      sidefire_2025$dir_notmatch_26[i] = 1
+    }
+  } 
+  # count name contains EB but road link name does not
+  else if (isTRUE(grepl("EB", sidefire_2025$LINKNAME[i]))) {
+    if (isFALSE(grepl('EB', sidefire_2025$nearlink_name_26[i]))) {
+      sidefire_2025$dir_notmatch_26[i] = 1
+    }
+  }
+  # count name contains WB but road link name does not
+  else if (isTRUE(grepl("WB", sidefire_2025$LINKNAME[i]))) {
+    if (isFALSE(grepl('WB', sidefire_2025$nearlink_name_26[i]))) {
+      sidefire_2025$dir_notmatch_26[i] = 1
+    }
+  } 
+}
+
+for (i in 1:nrow(sidefire_2025)) {
+  # count name does not contain NB but road link name has
+  if (isTRUE(grepl("NB", sidefire_2025$nearlink_name_26[i]))) {
+    if (isFALSE(grepl('NB', sidefire_2025$LINKNAME[i]))) {
+      sidefire_2025$dir_notmatch_26[i] = 1
+    }
+  } 
+  # count name does not contain SB but road link name has
+  else if (isTRUE(grepl("SB", sidefire_2025$nearlink_name_26[i]))) {
+    if (isFALSE(grepl('SB', sidefire_2025$LINKNAME[i]))) {
+      sidefire_2025$dir_notmatch_26[i] = 1
+    }
+  }
+  # count name does not contain EB but road link name has
+  else if (isTRUE(grepl("EB", sidefire_2025$nearlink_name_26[i]))) {
+    if (isFALSE(grepl('EB', sidefire_2025$LINKNAME[i]))) {
+      sidefire_2025$dir_notmatch_26[i] = 1
+    }
+  }
+  # count name does not contain WB but road link name has
+  else if (isTRUE(grepl("WB", sidefire_2025$nearlink_name_26[i]))) {
+    if (isFALSE(grepl('WB', sidefire_2025$LINKNAME[i]))) {
+      sidefire_2025$dir_notmatch_26[i] = 1
+    }
+  }
+}
+
+# check if there is any SW, SE, etc.
+# manual checked, all of those SE, SW, NE, NW are correct
+length(which(sidefire_2025$dir_notmatch_26 == 1))
+
+# mark ramp not match (1)
+sidefire_2025$ramp_notmatch_26 = 0
+for (i in 1:nrow(sidefire_2025)) {
+  # if count link is ramp while road link is not ramp
+  if (isTRUE(grepl('Ramp', sidefire_2025$LINKNAME[i])) | isTRUE(grepl('ramp', sidefire_2025$LINKNAME[i]))) {
+    if (isFALSE(grepl('RAMP', sidefire_2025$nearlink_name_26[i]))) {
+      sidefire_2025$ramp_notmatch_26[i] = 1
+    }
+  }
+  # if road link is ramp while count link is not ramp
+  if (isTRUE(grepl('RAMP', sidefire_2025$nearlink_name_26[i]))) {
+    if (isFALSE(grepl('Ramp', sidefire_2025$LINKNAME[i])) & isFALSE(grepl('ramp', sidefire_2025$LINKNAME[i]))) {
+      sidefire_2025$ramp_notmatch_26[i] = 1
+    }
+  }
+}
+length(which(sidefire_2025$ramp_notmatch_26 == 1))
+
+# mark name not match (1) strsplit string can be '-', '.', ' ', in count link name 
+# --> logic wrong. some I highway can also called IH, US highway has its nick name (like Spur)
+# re-write this part of code
+# first use strict match
+sidefire_2025$name_strictnotmatch_26 = 0
+for (i in 1:nrow(sidefire_2025)) {
+  # for count links that are not ramp
+  if (isFALSE(grepl('Ramp', sidefire_2025$LINKNAME[i])) & isFALSE(grepl('ramp', sidefire_2025$LINKNAME[i]))) {
+    if (strsplit(sidefire_2025$nearlink_name_26[i],"[-. ]+")[[1]][1] != strsplit(sidefire_2025$LINKNAME[i], '[-. ]+')[[1]][1]) {
+      sidefire_2025$name_strictnotmatch_26[i] = 1
+      }
+  }
+}
+length(which(sidefire_2025$name_strictnotmatch_26 == 1))
+
+# find counters that match with link according to distance, funcl, dir, name
+sidefire_2025$link_notmatch_26 = sidefire_2025$dir_notmatch_26 + sidefire_2025$name_strictnotmatch_26 + 
+  sidefire_2025$funcl_notmatch + sidefire_2025$ramp_notmatch_26
+length(which(sidefire_2025$link_notmatch_26 >= 1))
+
+sidefire_match_2025 = sidefire_2025[which(sidefire_2025$link_notmatch_26 == 0),]
+
+# find sidefire that within the taz
+within_matrix <- st_intersects(sidefire_match_2025, taz_2026, sparse = FALSE) 
+any_inside <- apply(within_matrix, 1, any) # apply to all TAZs
+sidefire_inTaz_2025 <- sidefire_match_2025[any_inside, ]
+
+# clear environment
+rm(within_matrix, any_inside, i, NB_match, SB_match, WB_match, EB_match, distances, nearest_line_indices)
+rm(inside)
+rm(nearline, points_inside)
+
+
+# check the consistency with Francisco
+sidefire_2025_Frcsc = read.csv('~/0_ModelDataDevelopment/20250410_capacity_recalculation/RoadNetwork_2026/Sensor_count/ClosestLinksToSidefires_FromFrancisco.csv', header = T)
+sidefire_2025_Frcsc$consistency = 0
+for (i in 1:nrow(sidefire_2025_Frcsc)) {
+  if (length(which(sidefire_inTaz_2025$ID == sidefire_2025_Frcsc$Sidefire_ID[i])) > 0) {
+    sidefire_2025_Frcsc$consistency[i] = 1
+  }
+}
+length(which(sidefire_2025_Frcsc$consistency == 0))
+
+sidefire_inTaz_2025$consistency = 0
+for (i in 1:nrow(sidefire_inTaz_2025)) {
+  if (length(which(sidefire_2025_Frcsc$Sidefire_ID == sidefire_inTaz_2025$ID[i])) > 0) {
+    sidefire_inTaz_2025$consistency[i] = 1
+  }
+}
+length(which(sidefire_inTaz_2025$consistency == 0))
+
+
+# check if it is wrong in my methods or it is his error --> April 22
+# find sensors that I exclude & corresponding links
+sensors_exclude_rtu = sidefire_2025_Frcsc$Sidefire_ID[which(sidefire_2025_Frcsc$consistency == 0)]
+sidefire_2025$rtu_exclude = 0
+for (i in 1:nrow(sidefire_2025)) {
+  if (length(which(sensors_exclude_rtu == sidefire_2025$ID[i])) > 0) {
+    sidefire_2025$rtu_exclude[i] = 1
+  }
+}
+sensors_exclude_rtu = sidefire_2025[which(sidefire_2025$rtu_exclude == 1),]
+
+# find sensors that I include but Frcsc exclude & corresponding links
+sensors_exclude_frcsc = sidefire_inTaz_2025$ID[which(sidefire_inTaz_2025$consistency == 0)]
+sidefire_2025$frcsc_exclude = 0
+for (i in 1:nrow(sidefire_2025)) {
+  if (length(which(sensors_exclude_frcsc == sidefire_2025$ID[i])) > 0) {
+    sidefire_2025$frcsc_exclude[i] = 1
+  }
+}
+sensors_exclude_frcsc = sidefire_2025[which(sidefire_2025$frcsc_exclude == 1),]
+
+write.csv(sensors_exclude_frcsc, '~/0_ModelDataDevelopment/20250410_capacity_recalculation/RoadNetwork_2026/Sensor_count/sidefire_2025_excludedbyFrancisco.csv', row.names = F)
+write.csv(sensors_exclude_rtu, '~/0_ModelDataDevelopment/20250410_capacity_recalculation/RoadNetwork_2026/Sensor_count/sidefire_2025_excludedbyRTu.csv', row.names = F)
+
+# plot matched sidefire detectors
+library('ggplot2')
+library('ggmap')
+library('tmap')
+library('osmdata')
+
+# use 'tmap' for interactive map
+# Get OSM background tiles
+tmap_mode("view")  # for interactive; use "plot" for static
+
+tm_basemap("OpenStreetMap") +
+  tm_shape(taz_2026) + tm_polygons(fill = "blue", fill_alpha = 0.4) +
+  tm_shape(roadlink_2026) + tm_lines(col = "grey", lwd = 1) +
+  tm_shape(sidefire_inTaz_2025) + tm_dots(col = "red", size = 1.5)
+
+# use leaflet
+install.packages('leaflet')
+library('leaflet')
+library('sf')
+
+leaflet() %>%
+  addTiles() %>%
+  addPolygons(data = st_transform(taz_2026, crs = 4326), color = 'blue', fillOpacity = 0.3, weight = 0.2) %>%
+  addPolylines(data = st_transform(roadlink_2026, crs = 4326), color = "grey", weight = 1.5) %>%
+  addCircleMarkers(data = st_transform(sidefire_inTaz_2025, crs = 4326), color = "red", radius = 1.5)
+
+# use ggmap for ggplot style map
+# center = c(lon = mean(st_coordinates(sidefire_inTaz_2025)[,1]), lat = mean(st_coordinates(sidefire_inTaz_2025)[,2]))
+# Get OSM basemap
+# register_google(key = 'AIzaSyC9aOym9ABm41MFrlKSozxrEm6g3eyTFOs')
+# map <- get_map(location = center, zoom = 15, source = "google")
+# Plot using ggmap and ggplot2
+# ggmap(map) +
+#   geom_polygon(data = taz_2026, aes(x = lon, y = lat),
+#                fill = "blue", alpha = 0.3, color = "blue") +
+#    geom_path(data = roadlink_2026, aes(x = lon, y = lat),
+#             color = "green", linewidth = 1.5) +
+#   geom_point(data = sidefire_inTaz_2025, aes(x = lon, y = lat),
+#             color = "red", size = 3)
+
