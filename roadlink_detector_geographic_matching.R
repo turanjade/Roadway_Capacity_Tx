85fb8f2fcd56a8c7a7a610fe068bef0837d93dcd
turanjade <ran.tu@alumni.utoronto.ca>
2025-04-24 16:04:06 -0500

    revision on DIR and ramp matching

--- roadlink_detector_geographic_matching.R
+++ roadlink_detector_geographic_matching.R
@@ -1,6 +1,8 @@
 library('arcgisbinding')
 library('sf')
 library('dplyr')
+library('openxlsx')
+
 
 setwd("C:\\Users\\rtu\\OneDrive - The North Central Texas Council of Governments\\Documents\\0_ModelDataDevelopment")
 
@@ -22,111 +24,93 @@ nearest_lines <- roadlink_2026[nearest_line_indices, ]
 
 sidefire_2025$nearlink_id_26 = nearest_lines$ID
 sidefire_2025$nearlink_name_26 = nearest_lines$STREET
+sidefire_2025$nearlink_dir_26 = nearest_lines$FAC_DIR
 sidefire_2025$nearlink_funcl_26 = nearest_lines$FUNCL
 
 # calculate the nearest distance
 distances <- st_distance(sidefire_2025, nearest_lines, by_element = TRUE)
 sidefire_2025$distance_meters_26 <- as.numeric(distances)
 
-### check Francisco's method
-# mark distance > 100ft (1)
+# mark distance > 100ft (1) # only choose the closest, need to find more links in the next round
 sidefire_2025$distance_notmatch_26 = 0
 sidefire_2025$distance_notmatch_26[which(sidefire_2025$distance_meters_26 > 30.48)] = 1
 length(which(sidefire_2025$distance_notmatch_26 == 1))
+unique(sidefire_2025$FUNCL[which(sidefire_2025$funcl_notmatch == 0 & sidefire_2025$distance_notmatch_26 == 0)])
+
+# mark sidefire FUNCL >= 8
+sidefire_2025$managelane = sidefire_2025$FUNCL >= 8
 
 # mark link FUNCL not match (1)
 sidefire_2025$funcl_notmatch = 0
 for (i in 1:nrow(sidefire_2025)) {
-  if (sidefire_2025$FUNCL[i] != roadlink_2026$FUNCL[which(roadlink_2026$ID == sidefire_2025$nearlink_id_26[i])]) {
+  if (sidefire_2025$FUNCL[i] != sidefire_2025$nearlink_funcl_26[i]) {
     sidefire_2025$funcl_notmatch[i] = 1
   }
 }
 length(which(sidefire_2025$funcl_notmatch == 1))
+unique(sidefire_2025$FUNCL[which(sidefire_2025$funcl_notmatch == 0 & sidefire_2025$distance_notmatch_26 == 0)])
 
 # mark direction not match (1)
 # NB, SB, EB, WB
-NB_match = grepl(" NB", sidefire_2025$LINKNAME) * grepl(" NB", sidefire_2025$near_line_name_26)
-SB_match = grepl(" SB", sidefire_2025$LINKNAME) * grepl(" SB", sidefire_2025$near_line_name_26)
-EB_match = grepl(" EB", sidefire_2025$LINKNAME) * grepl(" EB", sidefire_2025$near_line_name_26)
-WB_match = grepl(" WB", sidefire_2025$LINKNAME) * grepl(" WB", sidefire_2025$near_line_name_26)
+# NB_match = grepl(" NB", sidefire_2025$LINKNAME) * grepl(" NB", sidefire_2025$near_line_name_26)
+# SB_match = grepl(" SB", sidefire_2025$LINKNAME) * grepl(" SB", sidefire_2025$near_line_name_26)
+# EB_match = grepl(" EB", sidefire_2025$LINKNAME) * grepl(" EB", sidefire_2025$near_line_name_26)
+# WB_match = grepl(" WB", sidefire_2025$LINKNAME) * grepl(" WB", sidefire_2025$near_line_name_26)
 
-# check if NB, SB, EB, WB match
+# check if NB, SB, EB, WB match. can be both linkname and dir
 sidefire_2025$dir_notmatch_26 = 0
 for (i in 1:nrow(sidefire_2025)) {
   # count name contains NB but road link name does not
-  if (isTRUE(grepl("NB", sidefire_2025$LINKNAME[i]))) {
-    if (isFALSE(grepl('NB', sidefire_2025$nearlink_name_26[i]))) {
+  if (isTRUE(grepl("NB", sidefire_2025$LINKNAME[i])) | isTRUE(grepl('N', sidefire_2025$DIR[i], ignore.case = T))) {
+    if (isFALSE(grepl('NB', sidefire_2025$nearlink_name_26[i])) & isFALSE(grepl('N', sidefire_2025$nearlink_dir_26[i], ignore.case = T))) {
       sidefire_2025$dir_notmatch_26[i] = 1
     }
   } 
   # count name contains SB but road link name does not
-  else if (isTRUE(grepl("SB", sidefire_2025$LINKNAME[i]))) {
-    if (isFALSE(grepl('SB', sidefire_2025$nearlink_name_26[i]))) {
+  else if (isTRUE(grepl("SB", sidefire_2025$LINKNAME[i])) | isTRUE(grepl('S', sidefire_2025$DIR[i], ignore.case = T))) {
+    if (isFALSE(grepl('SB', sidefire_2025$nearlink_name_26[i])) & isFALSE(grepl('S', sidefire_2025$nearlink_dir_26[i], ignore.case = T))) {
       sidefire_2025$dir_notmatch_26[i] = 1
     }
   } 
   # count name contains EB but road link name does not
-  else if (isTRUE(grepl("EB", sidefire_2025$LINKNAME[i]))) {
-    if (isFALSE(grepl('EB', sidefire_2025$nearlink_name_26[i]))) {
+  else if (isTRUE(grepl("EB", sidefire_2025$LINKNAME[i])) | isTRUE(grepl('E', sidefire_2025$DIR[i], ignore.case = T))) {
+    if (isFALSE(grepl('EB', sidefire_2025$nearlink_name_26[i])) & isFALSE(grepl('E', sidefire_2025$nearlink_dir_26[i], ignore.case = T))) {
       sidefire_2025$dir_notmatch_26[i] = 1
     }
   }
   # count name contains WB but road link name does not
-  else if (isTRUE(grepl("WB", sidefire_2025$LINKNAME[i]))) {
-    if (isFALSE(grepl('WB', sidefire_2025$nearlink_name_26[i]))) {
+  else if (isTRUE(grepl("WB", sidefire_2025$LINKNAME[i])) | isTRUE(grepl('W', sidefire_2025$DIR[i], ignore.case = T))) {
+    if (isFALSE(grepl('WB', sidefire_2025$nearlink_name_26[i])) & isFALSE(grepl('W', sidefire_2025$nearlink_dir_26[i], ignore.case = T))) {
       sidefire_2025$dir_notmatch_26[i] = 1
     }
   } 
-}
-
-for (i in 1:nrow(sidefire_2025)) {
-  # count name does not contain NB but road link name has
-  if (isTRUE(grepl("NB", sidefire_2025$nearlink_name_26[i]))) {
-    if (isFALSE(grepl('NB', sidefire_2025$LINKNAME[i]))) {
-      sidefire_2025$dir_notmatch_26[i] = 1
-    }
-  } 
-  # count name does not contain SB but road link name has
-  else if (isTRUE(grepl("SB", sidefire_2025$nearlink_name_26[i]))) {
-    if (isFALSE(grepl('SB', sidefire_2025$LINKNAME[i]))) {
-      sidefire_2025$dir_notmatch_26[i] = 1
-    }
-  }
-  # count name does not contain EB but road link name has
-  else if (isTRUE(grepl("EB", sidefire_2025$nearlink_name_26[i]))) {
-    if (isFALSE(grepl('EB', sidefire_2025$LINKNAME[i]))) {
-      sidefire_2025$dir_notmatch_26[i] = 1
-    }
-  }
-  # count name does not contain WB but road link name has
-  else if (isTRUE(grepl("WB", sidefire_2025$nearlink_name_26[i]))) {
-    if (isFALSE(grepl('WB', sidefire_2025$LINKNAME[i]))) {
-      sidefire_2025$dir_notmatch_26[i] = 1
-    }
-  }
 }
 
 # check if there is any SW, SE, etc.
-# manual checked, all of those SE, SW, NE, NW are correct
-length(which(sidefire_2025$dir_notmatch_26 == 1))
+# manual checked, all of those SE, SW, NE, NW are correct -- do not have to check
+length(which(sidefire_2025$dir_notmatch_26 == 1 | sidefire_2025$funcl_notmatch == 1))
+unique(sidefire_2025$FUNCL[which(sidefire_2025$dir_notmatch_26 == 0 & sidefire_2025$funcl_notmatch == 0 & sidefire_2025$distance_notmatch_26 == 0)])
 
 # mark ramp not match (1)
 sidefire_2025$ramp_notmatch_26 = 0
 for (i in 1:nrow(sidefire_2025)) {
   # if count link is ramp while road link is not ramp
-  if (isTRUE(grepl('Ramp', sidefire_2025$LINKNAME[i])) | isTRUE(grepl('ramp', sidefire_2025$LINKNAME[i]))) {
-    if (isFALSE(grepl('RAMP', sidefire_2025$nearlink_name_26[i]))) {
+  if (isTRUE(grepl('ramp', sidefire_2025$LINKNAME[i], ignore.case = T))) {
+    if (isFALSE(grepl('RAMP', sidefire_2025$nearlink_name_26[i], ignore.case = T))) {
       sidefire_2025$ramp_notmatch_26[i] = 1
     }
   }
   # if road link is ramp while count link is not ramp
-  if (isTRUE(grepl('RAMP', sidefire_2025$nearlink_name_26[i]))) {
-    if (isFALSE(grepl('Ramp', sidefire_2025$LINKNAME[i])) & isFALSE(grepl('ramp', sidefire_2025$LINKNAME[i]))) {
+  if (isTRUE(grepl('RAMP', sidefire_2025$nearlink_name_26[i], ignore.case = T))) {
+    if (isTRUE(grepl('ramp', sidefire_2025$LINKNAME[i], ignore.case = T))) {
       sidefire_2025$ramp_notmatch_26[i] = 1
     }
   }
 }
-length(which(sidefire_2025$ramp_notmatch_26 == 1))
+length(which(sidefire_2025$dir_notmatch_26 == 1 | sidefire_2025$funcl_notmatch == 1 | sidefire_2025$ramp_notmatch_26 == 1))
+unique(sidefire_2025$FUNCL[which(sidefire_2025$dir_notmatch_26 == 0 & sidefire_2025$funcl_notmatch == 0 & 
+                                   sidefire_2025$ramp_notmatch_26 == 0 & sidefire_2025$distance_notmatch_26 == 0)])
+
 
 # mark name not match (1) strsplit string can be '-', '.', ' ', in count link name 
 # --> logic wrong. some I highway can also called IH, US highway has its nick name (like Spur)
@@ -135,13 +119,19 @@ length(which(sidefire_2025$ramp_notmatch_26 == 1))
 sidefire_2025$name_strictnotmatch_26 = 0
 for (i in 1:nrow(sidefire_2025)) {
   # for count links that are not ramp
-  if (isFALSE(grepl('Ramp', sidefire_2025$LINKNAME[i])) & isFALSE(grepl('ramp', sidefire_2025$LINKNAME[i]))) {
+  if (isFALSE(grepl('ramp', sidefire_2025$LINKNAME[i], ignore.case = T)) & 
+      isFALSE(grepl('ramp', sidefire_2025$nearlink_name_26[i], ignore.case = T))) {
     if (strsplit(sidefire_2025$nearlink_name_26[i],"[-. ]+")[[1]][1] != strsplit(sidefire_2025$LINKNAME[i], '[-. ]+')[[1]][1]) {
       sidefire_2025$name_strictnotmatch_26[i] = 1
       }
   }
 }
-length(which(sidefire_2025$name_strictnotmatch_26 == 1))
+length(which(sidefire_2025$dir_notmatch_26 == 1 | sidefire_2025$funcl_notmatch == 1 | 
+               sidefire_2025$ramp_notmatch_26 == 1 | sidefire_2025$name_strictnotmatch_26 == 1))
+unique(sidefire_2025$FUNCL[which(sidefire_2025$dir_notmatch_26 == 0 & sidefire_2025$funcl_notmatch == 0 &
+                                   sidefire_2025$ramp_notmatch_26 == 0 & sidefire_2025$name_strictnotmatch_26 == 0 & 
+                                   sidefire_2025$distance_notmatch_26 == 0)])
+
 
 # find counters that match with link according to distance, funcl, dir, name
 sidefire_2025$link_notmatch_26 = sidefire_2025$dir_notmatch_26 + sidefire_2025$name_strictnotmatch_26 + 
@@ -160,7 +150,6 @@ rm(within_matrix, any_inside, i, NB_match, SB_match, WB_match, EB_match, distanc
 rm(inside)
 rm(nearline, points_inside)
 
-library('openxlsx')
 
 # check the consistency with Francisco
 sidefire_2025_Frcsc = read.xlsx('~/0_ModelDataDevelopment/20250410_capacity_recalculation/RoadNetwork_2026/Sensor_count/ClosestLinksToSidefires_FromFrancisco.xlsx', sheet = 'FromFrancisco_updated0423')
